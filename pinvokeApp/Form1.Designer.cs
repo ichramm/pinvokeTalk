7c02c95fdf757ea5ba4f061504ef205323a1a7d6
ichramm <jramirez@inconcertcc.com>
2014-03-09 18:02:30 -0300

    example2 - calling convention

--- pinvokeApp/Form1.Designer.cs
+++ pinvokeApp/Form1.Designer.cs
@@ -30,14 +30,8 @@ namespace pinvokeApp
 		{
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
-			this.lblLenUnicode = new System.Windows.Forms.Label();
 			this.lblLenAnsi = new System.Windows.Forms.Label();
-			this.lblLenCrazy1 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.lblLenCrazy2 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// textBox1
@@ -59,37 +53,15 @@ namespace pinvokeApp
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Text";
 			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(12, 136);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(156, 24);
-			this.label2.TabIndex = 2;
-			this.label2.Text = "Length (Unicode)";
-			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label3.Location = new System.Drawing.Point(12, 88);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(122, 24);
+			this.label3.Size = new System.Drawing.Size(68, 24);
 			this.label3.TabIndex = 3;
-			this.label3.Text = "Length (Ansi)";
-			// 
-			// lblLenUnicode
-			// 
-			this.lblLenUnicode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.lblLenUnicode.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblLenUnicode.ForeColor = System.Drawing.Color.YellowGreen;
-			this.lblLenUnicode.Location = new System.Drawing.Point(288, 121);
-			this.lblLenUnicode.Name = "lblLenUnicode";
-			this.lblLenUnicode.Size = new System.Drawing.Size(107, 39);
-			this.lblLenUnicode.TabIndex = 4;
-			this.lblLenUnicode.Text = "0";
-			this.lblLenUnicode.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.label3.Text = "Length";
 			// 
 			// lblLenAnsi
 			// 
@@ -103,63 +75,13 @@ namespace pinvokeApp
 			this.lblLenAnsi.Text = "0";
 			this.lblLenAnsi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
-			// lblLenCrazy1
-			// 
-			this.lblLenCrazy1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.lblLenCrazy1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblLenCrazy1.ForeColor = System.Drawing.Color.YellowGreen;
-			this.lblLenCrazy1.Location = new System.Drawing.Point(288, 169);
-			this.lblLenCrazy1.Name = "lblLenCrazy1";
-			this.lblLenCrazy1.Size = new System.Drawing.Size(107, 39);
-			this.lblLenCrazy1.TabIndex = 7;
-			this.lblLenCrazy1.Text = "0";
-			this.lblLenCrazy1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(12, 184);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(147, 24);
-			this.label5.TabIndex = 6;
-			this.label5.Text = "Length (Crazy 1)";
-			// 
-			// lblLenCrazy2
-			// 
-			this.lblLenCrazy2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.lblLenCrazy2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblLenCrazy2.ForeColor = System.Drawing.Color.YellowGreen;
-			this.lblLenCrazy2.Location = new System.Drawing.Point(288, 216);
-			this.lblLenCrazy2.Name = "lblLenCrazy2";
-			this.lblLenCrazy2.Size = new System.Drawing.Size(107, 39);
-			this.lblLenCrazy2.TabIndex = 9;
-			this.lblLenCrazy2.Text = "0";
-			this.lblLenCrazy2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label6.Location = new System.Drawing.Point(12, 231);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(147, 24);
-			this.label6.TabIndex = 8;
-			this.label6.Text = "Length (Crazy 2)";
-			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(407, 264);
-			this.Controls.Add(this.lblLenCrazy2);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.lblLenCrazy1);
-			this.Controls.Add(this.label5);
+			this.ClientSize = new System.Drawing.Size(407, 136);
 			this.Controls.Add(this.lblLenAnsi);
-			this.Controls.Add(this.lblLenUnicode);
 			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
 			this.Name = "Form1";
@@ -173,14 +95,8 @@ namespace pinvokeApp
 
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label lblLenUnicode;
 		private System.Windows.Forms.Label lblLenAnsi;
-		private System.Windows.Forms.Label lblLenCrazy1;
-		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.Label lblLenCrazy2;
-		private System.Windows.Forms.Label label6;
 	}
 }
 
--- pinvokeApp/NativeMethods.cs
+++ pinvokeApp/NativeMethods.cs
@@ -9,33 +9,9 @@ namespace pinvokeApp
 {
 	class NativeMethods
 	{
-		// Default: Should be ANSI
 		[DllImport("pinvokeLib.dll"
-			, CallingConvention = CallingConvention.Cdecl
-			, EntryPoint = "GetStringLength"
+			//, CallingConvention = CallingConvention.Cdecl
 		)]
-		public static extern uint GetStringLength1(string s);
-
-		// Unicode
-		[DllImport("pinvokeLib.dll"
-			, CallingConvention = CallingConvention.Cdecl
-			, EntryPoint = "GetStringLength"
-			, CharSet           = CharSet.Unicode
-		)]
-		public static extern uint GetStringLength2(string s);
-
-		// Mixed
-		[DllImport("pinvokeLib.dll"
-			, CallingConvention = CallingConvention.Cdecl
-			, CharSet           = CharSet.Unicode
-			, EntryPoint        = "GetStringLengthA"
-		)]
-		public static extern uint GetStringLengthCrazy1(string s);
-
-		[DllImport("pinvokeLib.dll"
-			, CallingConvention = CallingConvention.Cdecl
-			, EntryPoint = "GetStringLengthW"
-		)]
-		public static extern uint GetStringLengthCrazy2(string s);
+		public static extern uint GetStringLength(string s);
 	}
 }
